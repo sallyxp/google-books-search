0edb18a1bcea41172928e2a21ba6e9cb0c32af75
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-24 18:35:09 +0100

    set up server, package.json and link to googlebooks

---
+++ server.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'));
+}
+
+// Define API routes
+app.use(routes);
+
+// wildcard handler for all pages
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+
+});
+
+//testing connections
+mongoose.connection.on('connected', () => console.log('Connected to MongoDB Endpoint'));
+mongoose.connection.on('error', (err) => console.log(`Mongoose default connection error: ${err}`));
+
+// Define API routes
+app.use(routes);
+
+// Send every other request to the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
