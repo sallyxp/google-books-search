e6c1d32383c9a1a907b6201a91e7edd1a4ce7aa4
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-26 22:48:58 +0100

    created book model

--- models/Book.js
+++ models/Book.js
@@ -9,5 +9,22 @@ const bookSchema = new Schema({
     authors: {
         type: String, 
         required: true
-    }
-})
+    },
+   description: {
+       type: String, 
+       required: true
+   } ,
+   image: {
+       type: String, 
+       required: true
+   },
+   link: {
+       type: String, 
+       required: true
+   }
+
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
