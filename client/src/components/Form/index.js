575411461432904e5165344c6a2adbbd8d395745
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-29 00:10:52 +0100

    created Form component

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,33 @@
+
+import React from "react";
+
+function Form({ q, handleInputChange, handleFormSubmit }) {
+    return (
+        <form>
+            <div className="form-group">
+                <label htmlFor="Query">
+                    <strong>Book</strong>
+                </label>
+                <input
+                    className="form-control"
+                    id="Title"
+                    type="text"
+                    value={q}
+                    placeholder="Enter Book title here"
+                    name="q"
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className="pull-right">
+                <button
+                    onClick={handleFormSubmit}
+                    type="submit"
+                    className="btn btn-lg btn-danger float-right"
+                >
+                    Search
+                </button>
+            </div>
+        </form>
+    );
+}
