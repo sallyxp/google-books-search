1f208cf8b0e05d9451f9a511b6d0c9c427bee61f
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-06-30 16:41:52 +0100

    had the wrong index.js in the wrong place - switched to correct

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
+
+const path = require("path");
+const googleRoutes = require("./google");
 const bookRoutes = require("./books");
 
 // Book routes
 router.use("/books", bookRoutes);
 
+// Google routes
+router.use("/google", googleRoutes);
+
+
 // For anything else, render the html page
 router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../../client/public/index.html"));
   });
 
 module.exports = router;
