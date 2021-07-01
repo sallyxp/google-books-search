5eed5e796b2e4d2e91c10aba9b458f8ae67a00e6
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-07-01 17:50:02 +0100

    added Nav back

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -11,6 +11,7 @@ mongoose.connect(
 const bookSeed = [
   {
     title: "The Dead Zone",
+    subtitle: "Horror book",
     authors: "Stephen King",
     description:
       "A number-one national best seller about a man who wakes up from a five-year coma able to see people's futures and the terrible fate awaiting mankind in The Dead Zone - a \"compulsive page-turner\" (The Atlanta Journal-Constitution). Johnny Smith awakens from a five-year coma after his car accident and discovers that he can see people's futures and pasts when he touches them. Many consider his talent a gift; Johnny feels cursed. His fiancée married another man during his coma, and people clamor for him to solve their problems. When Johnny has a disturbing vision after he shakes the hand of an ambitious and amoral politician, he must decide if he should take drastic action to change the future. The Dead Zone is a \"faultlessly paced...continuously engrossing\" (Los Angeles Times) novel of second sight.",
